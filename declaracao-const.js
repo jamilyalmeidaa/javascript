2d3edd760fb757523fb3ecc1f0d9601339aeebac
jamilyalmeidaa <jamilyalmeidasanches2@gmail.com>
2026-09-04 18:10:32 -0300

    const

---
+++ declaracao-const.js
@@ -0,0 +1,9 @@
+function exemploConst() {
+    const x = 10; 
+   console.log(x); // 10  
+   // x = 20; // Isso causará um erro, pois 'x' é uma constante e não pode ser reatribuída
+    if (true) {
+         const y = 30;
+    console.log(y); // 30
+    }
+    console.log(x); ();
