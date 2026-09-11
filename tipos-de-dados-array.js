d5cde86c637672abccca517b6326c89584abde25
jamilyalmeidaa <jamilyalmeidasanches2@gmail.com>
2026-09-11 15:21:43 -0300

    add tipo de dado

---
+++ tipos-de-dados-array.js
@@ -0,0 +1,3 @@
+let frutas = ["maca", "banana", "laranja"]; // Array de strings
+let numeros = [1, 2, 3, 4, 5,]; // Array de numeros 
+let misturado = ["texto", 42, true]; // Array com diferentes tipos de dados
