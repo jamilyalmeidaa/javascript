18f303d43ad3f5961693f9ba4613da71eb666a92
jamilyalmeidaa <jamilyalmeidasanches2@gmail.com>
2026-09-11 15:23:52 -0300

    add tipo dado stringer

---
+++ tipos-de-dados-stringers.js
@@ -0,0 +1,3 @@
+let nome = 'ana'; // string com aspas duplas
+let sobrenome = 'silva'; // string com aspas simples
+let saudacao = `{ola, ${nome}!`; // template literal com interpolacao de variaveis 
