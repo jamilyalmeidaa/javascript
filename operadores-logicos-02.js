3260bcdd78aaa6b9f86f14a78e76f8fc34a23c00
jamilyalmeidaa <jamilyalmeidasanches2@gmail.com>
2026-09-22 15:28:44 -0300

    op logico 2

---
+++ operadores-logicos-02.js
@@ -0,0 +1,5 @@
+let a = (1 < 2) || (3 > 4)    // true || false = true 
+let b = (1 < 2) || (3 > 4)    // true || false = true 
+let c = (10 < 0) || (20 > 30)    // true && false = true 
+let d = (20 < 30) || (30 > 40)    // true && false = true 
+let e = (20 < 30) || (30 > 40)    // true && false = true 
