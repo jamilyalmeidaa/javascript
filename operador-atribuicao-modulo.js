e975e3a74cf65c1a5e683f332199324f49356a60
jamilyalmeidaa <jamilyalmeidasanches2@gmail.com>
2026-09-22 15:29:50 -0300

    op aTRI MODULO

---
+++ operador-atribuicao-modulo.js
@@ -0,0 +1,7 @@
+let totaldoces = 10;
+let criancas = 3;
+
+// queremos saber quantos doces sobram sobram apos dividir igualment 
+totaldoces %= criancas;
+
+console.log(totaldoces); // 1 -> pois 10 dividido por 3 sobra 1 
